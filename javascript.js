e115f36ca6d030d504f6f41ed3df571bacf067bb
Aleksandr Lavrov <sancho2140@gmail.com>
2022-11-06 22:20:53 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,95 @@
+// let paragraph
+
+// function listCreating() {
+//     const ulContainer = document.querySelector('.list-container')
+//     const listTask = document.createElement('li');
+//     const container = document.createElement('div')
+//     const tick = document.createElement('input') 
+//     const button = document.createElement('button')
+
+//     container.classList.add('div')
+
+//     tick.type = 'checkbox'
+//     tick.classList.add('checkbox')
+//     tick.addEventListener('change', ()=> {
+//         container.classList.add('green')
+//     })
+
+//     paragraph = document.createElement('p')
+
+//     button.classList.add('delete')
+//     button.textContent = 'delete'
+//     button.addEventListener('click', () => {
+//         listTask.remove()
+//     })
+
+//     container.append(tick,paragraph,button)
+//     listTask.append(container)
+//     ulContainer.append(listTask)
+// }
+
+
+// function addingGoal() {
+//     let bank = document.querySelector('.task-text').value
+
+//     if (bank === '') {
+//         return
+//     } else {
+//         listCreating()
+//         paragraph.textContent = bank
+
+//         bank = ''
+//     }
+
+// }
+
+
+let textField
+
+function listCreating() {
+    const ulContainer = document.querySelector('.list-container')
+    const listTask = document.createElement('li');
+    
+    textField = document.createElement('input')
+    textField.type = 'text'
+    textField.setAttribute('readonly', 'readonly')
+
+    const buttonContainer = document.createElement('div')
+
+    const deleteButton = document.createElement('button')
+    deleteButton.classList.add('delete')
+    deleteButton.textContent = 'delete'
+    deleteButton.addEventListener('click', () => {
+        listTask.remove()
+    })
+
+    const editButton = document.createElement('button')
+    editButton.classList.add('edit')
+    editButton.textContent = 'Edit'
+    editButton.addEventListener('click', ()=>{
+        if (editButton.textContent.toLowerCase() == 'edit') {
+            textField.removeAttribute('readonly')
+            textField.focus()
+            editButton.textContent = 'Save'
+        } else {
+            textField.setAttribute('readonly', 'readonly')
+            editButton.textContent = 'Edit'
+        }
+    })
+
+    buttonContainer.append(editButton, deleteButton)
+    listTask.append(textField,buttonContainer)
+    ulContainer.append(listTask)
+}
+
+
+function addingGoal() {
+    let bank = document.querySelector('.task-text')
+    if (bank.value === '') {
+        return
+    } else {
+        listCreating()
+        textField.value = bank.value
+        bank.value = ''
+    }
+}
