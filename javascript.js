637f62753d00097a8ea1b611891f032d1b8f62d8
Aleksandr Lavrov <sancho2140@gmail.com>
2024-04-10 01:00:23 +0300

    small locla storage update 2024

--- javascript.js
+++ javascript.js
@@ -1,12 +1,13 @@
-function listCreating(value) {
+function listCreating(obj) {
+    const listId = obj.id;
+
     const ulContainer = document.querySelector('.list-container')
     const listTask = document.createElement('li');
-    
-    const textField = document.createElement('input')
-    textField.value = value
-    textField.type = 'text'
-    textField.setAttribute('readonly', 'readonly')
-    textField.classList.add('li-input')
+    const textField = document.createElement('input');
+    textField.value = obj.value;
+    textField.type = 'text';
+    textField.setAttribute('readonly', 'readonly');
+    textField.classList.add('li-input');
 
     const buttonContainer = document.createElement('div')
 
@@ -14,7 +15,11 @@ function listCreating(value) {
     deleteButton.classList.add('delete')
     deleteButton.textContent = 'delete'
     deleteButton.addEventListener('click', () => {
-        listTask.remove()
+        const goals = JSON.parse(localStorage.getItem("goals"));
+        const newArray = goals.filter(item => item.id !== listId);
+        localStorage.setItem("goals", JSON.stringify(newArray));
+
+        listTask.remove();
     })
 
     const editButton = document.createElement('button')
@@ -28,6 +33,8 @@ function listCreating(value) {
         } else {
             textField.setAttribute('readonly', 'readonly')
             editButton.textContent = 'Edit'
+
+            changeList(listId, textField.value);
         }
     })
 
@@ -43,7 +50,31 @@ function addingGoal() {
     if (bank.value === '') {
         return
     } else {
-        listCreating(bank.value)
-        bank.value = ''
+        const id = Math.random();
+
+        listCreating({id:id, value: bank.value});
+
+        const goals = JSON.parse(localStorage.getItem("goals")) ?? [];
+        goals.push({id:id, value: bank.value});
+        localStorage.setItem("goals", JSON.stringify(goals));
+
+        bank.value = '';
     }
+}
+
+function onLoad() {
+    const list = JSON.parse(localStorage.getItem("goals")) ?? [];
+    list.forEach(item => listCreating(item));
+}
+
+window.addEventListener('load', onLoad);
+
+function changeList(listId, value) {
+    const goals = JSON.parse(localStorage.getItem("goals"));
+    let newGoal = goals.find( item => item.id === listId);
+    newGoal.value = value;
+    const index = goals.findIndex(item => item.id === listId);
+    goals[index] = newGoal;
+
+    localStorage.setItem("goals", JSON.stringify(goals));
 }
